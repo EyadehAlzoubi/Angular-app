f1023fa954a38853ea3b92d1858965f936021a55
e.alzoubi <e.alzoubi@classera.com>
2024-12-18 07:38:12 +0300

    48. Dynamic CSS Styling with Class Bindings

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,6 +10,7 @@ import {type User} from './user.modle';
 export class UserComponent {
 
   @Input({required:true}) user!:User;
+  @Input({required:true}) selected!:boolean;
   @Output() select = new EventEmitter<string>();
 
   // select = output<string>();
